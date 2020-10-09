df1511ee3141c4bae69a40167b74e6ac94f3b965
Ameya Ganore <ameyaganore@gmail.com>
2020-10-09 11:40:26 +0530

    Updates

---
+++ precache-manifest.53feb04e3e91ca00ad8701473c08d2ef.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84d5b6a23663d3859ee85dcaf4a32b57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4de7683ee62d821727ca",
+    "url": "/static/css/main.63cd766a.chunk.css"
+  },
+  {
+    "revision": "b38158075cdde1fbadab",
+    "url": "/static/js/2.41fa3ee0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.41fa3ee0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4de7683ee62d821727ca",
+    "url": "/static/js/main.3d8e9747.chunk.js"
+  },
+  {
+    "revision": "ae51bc9f85e8278c1155",
+    "url": "/static/js/runtime-main.302436ce.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/static/media/bg (1).1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/static/media/spinner.235b9b3e.gif"
+  }
+]);
